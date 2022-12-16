737c81f570b0e1f389940b055ee6c36938d45ec5
GonzaS89 <gnzx89@gmail.com>
2022-12-16 19:16:25 -0300

    codigo base listo

--- main.js
+++ main.js
@@ -35,69 +35,127 @@ function reloj(){
 /* Funcion para determinar la localidad de posicion*/
 
 const selector = document.menu.selector;
+const selector2 = document.menu2.selector;
 
 selector.addEventListener('click', function(){
+    const opcionbase = selector[0]
     const florida = selector[1];
     const sanM = selector[2];
+   
+
 
     if((selector.value == 'florida') && (florida.disabled == false)){
-       
+        selector2.addEventListener('click', function(){
+            const opcionbase2 = selector2[0];
+            const alderetes = selector2[1];
+            const posse = selector2[2];
 
-    let horariosFloPo = [5.55,6.20,6.50,7.25,7.55,8.25,9.10,10.05,10.25,11,11.25,12,12.10,12.25,13.3,14,15,15.55,16.55,17.55,18.55,20,21.2,22.2]; /*Horarios desde Florida/Posse/San Miguel*/
-    let horariosFloAl = [4.40,5.35,6.1,6.25,6.4,6.5,7.25,7.4,7.5,8.45,9.2,9.5,10.3,11.3,12.3,12.4,13.15,14,15.5,15.5,16.2,16.5,17.5,18.1,18.3,19];/*Horarios desde Florida/Alderetes/San Miguel*/
+        if(selector2.value == 'alderetes'){
+
+    let horariosFloAl = [4.40,5.35,6.1,6.25,6.4,6.5,7.25,7.4,7.5,8.45,9.2,9.5,10.3,11.3,12.3,12.4,13.15,14,15.5,15.5,16.2,16.5,17.5,18.1,18.3,19,20,21,22];/*Horarios desde Florida/Alderetes/San Miguel*/
     let horariosEnEnterosFloAl=[];
-    let horariosEnEnterosFloPo=[];
-   ;
-   
-    const boton = document.getElementById('boton')
-
-    boton.addEventListener('click', function(){
+    console.log(horariosFloAl)
+ 
     let momentoActual = new Date();
     let hora = momentoActual.getHours();
     let minutos = momentoActual.getMinutes();
     let horaEnEnteros = (hora * 60) + minutos;
     
-    
-    
-
 /*/Convertimos los horarios en enteros y los mandamos a un nuevo array"*/
 
       for(let i=0 ; i < horariosFloAl.length ; i++){
-        let horasEnEnteros=  (Math.trunc(horariosFloAl[i])  * 60);
-        let minutosEnEnteros = ((horariosFloAl[i] - (Math.trunc(horariosFloAl[i])))*100);
-        let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
-        horariosEnEnterosFloAl.push(horaMinutosEnEnteros);
+               let elMasCercano; 
+               let anteriorPasado;
+               let horasEnEnteros=  (Math.trunc(horariosFloAl[i])  * 60);
+                let minutosEnEnteros = ((horariosFloAl[i] - (Math.trunc(horariosFloAl[i])))*100);
+                let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
+              horariosEnEnterosFloAl.push(horaMinutosEnEnteros);
+
+               /*Recorremos el array y buscamos coincidencias con el horario actual*/
+
+              for(i = 0; i < horariosEnEnterosFloAl.length; i++){
+                
+                let difHoraHorarios = horaEnEnteros - horariosEnEnterosFloAl[i];
+                let difHorariosHora = horariosEnEnterosFloAl[i] - horaEnEnteros;
+                if(difHoraHorarios > 0){
+                    anteriorPasado = difHoraHorarios;
+                }
+                document.resultados.pasado.value = `El colectivo pasó hace ${Math.ceil(anteriorPasado)} minutos`
+                
+                if((difHoraHorarios < 0) && (difHorariosHora < 15)){
+                    elMasCercano = difHorariosHora;
+                }
+                if((difHoraHorarios < 0) && (difHorariosHora < 30)){
+                    elMasCercano = difHorariosHora;
+                }
+                if((difHoraHorarios < 0) && (difHorariosHora < 45)){
+                    elMasCercano = difHorariosHora;    
+                }
+                if((difHoraHorarios < 0) && (difHorariosHora < 60)){
+                    elMasCercano = difHorariosHora;    
+                }
+                document.resultados.futuro.value = `El proximo colectivo viene en ${Math.ceil(elMasCercano)} minutos`
+           
+               }                
+
         }
-
-        for(let i=0 ; i < horariosFloPo.length ; i++){
-            let horasEnEnteros=  (Math.trunc(horariosFloPo[i])  * 60);
-            let minutosEnEnteros = ((horariosFloPo[i] - (Math.trunc(horariosFloAl[i])))*100);
-            let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
-            horariosEnEnterosFloPo.push(horaMinutosEnEnteros);
-            }
-   
-    /* ---------------------------------------------------------------------*/
-    
-    /*Recorremos el array y buscamos coincidencias con el horario actual*/
-    let elMasCercano;
-
-   for(i = 0; i < horariosEnEnterosFloAl.length; i++){
-    let difHoraHorarios = horaEnEnteros - horariosEnEnterosFloAl[i];
-    let difHorariosHora = horariosEnEnterosFloAl[i] - horaEnEnteros;
-    if((difHoraHorarios > 0)&&(difHoraHorarios < 60)){
-        elMasCercano = difHoraHorarios;
     }
-   } 
-   console.log(elMasCercano)
+    
+        if(selector2.value == 'posse'){
+            let horariosFloPo = [5.55,6.20,6.50,7.25,7.55,8.25,9.10,10.05,10.25,11,11.25,12,12.10,12.25,13.3,14,15,15.55,16.55,17.55,18.55,20,21.2,22.2]; /*Horarios desde Florida/Posse/San Miguel*/
 
-   document.resultados.alderete.value = `El proximo bondi por Alderetes viene en ${Math.ceil(elMasCercano)} minutos`
+          
 
-   
- 
-   
-   
-})
+            let horariosEnEnterosFloPo=[];
+
+            let momentoActual = new Date();
+            let hora = momentoActual.getHours();
+            let minutos = momentoActual.getMinutes();
+            let horaEnEnteros = (hora * 60) + minutos;
+            
+        /*/Convertimos los horarios en enteros y los mandamos a un nuevo array"*/
+        
+              for(let i=0 ; i < horariosFloPo.length ; i++){
+                       let elMasCercano; 
+                       let anteriorPasado;
+                       let horasEnEnteros=  (Math.trunc(horariosFloPo[i])  * 60);
+                        let minutosEnEnteros = ((horariosFloPo[i] - (Math.trunc(horariosFloPo[i])))*100);
+                        let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
+                      horariosEnEnterosFloPo.push(horaMinutosEnEnteros);
+        
+                       /*Recorremos el array y buscamos coincidencias con el horario actual*/
+        
+                      for(i = 0; i < horariosEnEnterosFloPo.length; i++){
+                        
+                        let difHoraHorarios = horaEnEnteros - horariosEnEnterosFloPo[i];
+                        let difHorariosHora = horariosEnEnterosFloPo[i] - horaEnEnteros;
+                        if(difHoraHorarios > 0){
+                            anteriorPasado = difHoraHorarios;
+                        }
+                        document.resultados.pasado.value = `El colectivo pasó hace ${Math.ceil(anteriorPasado)} minutos`
+                        
+                        if((difHoraHorarios < 0) && (difHorariosHora < 15)){
+                            elMasCercano = difHorariosHora;
+                        }
+                        if((difHoraHorarios < 0) && (difHorariosHora < 30)){
+                            elMasCercano = difHorariosHora;
+                        }
+                        if((difHoraHorarios < 0) && (difHorariosHora < 45)){
+                            elMasCercano = difHorariosHora;    
+                        }
+                        if((difHoraHorarios < 0) && (difHorariosHora < 60)){
+                            elMasCercano = difHorariosHora;    
+                        }
+                        document.resultados.futuro.value = `El proximo colectivo viene en ${Math.ceil(elMasCercano)} minutos`
+                   
+                       }                
+        
+                }
+        
+        }
+        })
 }
+    
 })
 
     
