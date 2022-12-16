35d12aa8f7b95621cb6b55795fff399cffd879bc
GonzaS89 <gnzx89@gmail.com>
2022-12-16 11:51:06 -0300

    Probando codigos

--- main.js
+++ main.js
@@ -44,9 +44,10 @@ selector.addEventListener('click', function(){
        
 
     let horariosFloPo = [5.55,6.20,6.50,7.25,7.55,8.25,9.10,10.05,10.25,11,11.25,12,12.10,12.25,13.3,14,15,15.55,16.55,17.55,18.55,20,21.2,22.2]; /*Horarios desde Florida/Posse/San Miguel*/
-    const horariosFloAl = [4.40,5.35,6.1,6.25,6.4,6.5,7.25,7.4,7.5,8.45,9.2,9.5,10.3,11.3,12.3,12.4,13.15,14,15.5,15.5,16.2,16.5,17.5,18.1,18.3,19];/*Horarios desde Florida/Alderetes/San Miguel*/
-    let horariosEnEnterosFloAl= [];
-    let horariosEnEnterosFloPo= [];
+    let horariosFloAl = [4.40,5.35,6.1,6.25,6.4,6.5,7.25,7.4,7.5,8.45,9.2,9.5,10.3,11.3,12.3,12.4,13.15,14,15.5,15.5,16.2,16.5,17.5,18.1,18.3,19];/*Horarios desde Florida/Alderetes/San Miguel*/
+    let horariosEnEnterosFloAl=[];
+    let horariosEnEnterosFloPo=[];
+   ;
    
     
 
@@ -59,30 +60,44 @@ selector.addEventListener('click', function(){
     let minutos = momentoActual.getMinutes();
     let horaEnEnteros = (hora * 60) + minutos;
     
+    
+    
 
 /*/Convertimos los horarios en enteros y los mandamos a un nuevo array"*/
 
-    for(horario of horariosFloAl){
-      horariosEnEnterosFloAl.push(horario)
-      console.log(horariosEnEnterosFloAl[1])
-    }
-    for(horario of horariosFloPo){
-            let horaEnEnteros=  (Math.trunc(horario)  * 60);
+      for(let i=0 ; i < horariosFloAl.length ; i++){
+        let horasEnEnteros=  (Math.trunc(horariosFloAl[i])  * 60);
+        let minutosEnEnteros = ((horariosFloAl[i] - (Math.trunc(horariosFloAl[i])))*100);
+        let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
+        horariosEnEnterosFloAl.push(horaMinutosEnEnteros);
+        }
+        
+     
+       
+      
+    
+             
+      
+
+      for(horario of horariosFloPo){
+            let horasEnEnteros=  (Math.trunc(horario)  * 60);
             let minutosEnEnteros = ((horario - (Math.trunc(horario)))*100);
-            let horaMinutosEnEnteros = horaEnEnteros + minutosEnEnteros;
+            let horaMinutosEnEnteros = horasEnEnteros + minutosEnEnteros;
             horariosEnEnterosFloPo.push(horaMinutosEnEnteros)
     }
     /* ---------------------------------------------------------------------*/
     
     /*Recorremos el array y buscamos coincidencias con el horario actual*/
 
+    let listaDifHorariosFloAl = [];
     for(horarioEnEnterosFloAl of horariosEnEnterosFloAl){
         let difHoraHorario = horaEnEnteros - horarioEnEnterosFloAl;
         let difHorarioHora = horarioEnEnterosFloAl - horaEnEnteros;
-        let listaHorarios = [];
-        listaHorarios.push(horariosEnEnterosFloAl);
-        console.log(listaHorarios)
+        listaDifHorariosFloAl.push(difHoraHorario);
+        
     }
+   
+   
 })
     }
 })
